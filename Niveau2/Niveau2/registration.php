399a1ac52e4ec354405747352711272d111d648f
nadhell <nadhell@live.com>
2023-02-14 14:24:11 +0100

    login OK

---
+++ Niveau2/Niveau2/registration.php
@@ -0,0 +1,102 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>ReSoC - Inscription</title> 
+        <meta name="author" content="Julien Falconnet">
+        <link rel="stylesheet" href="style.css"/>
+    </head>
+    <body>
+        <header>
+            <img src="resoc.jpg" alt="Logo de notre réseau social"/>
+            <nav id="menu">
+                <a href="news.php">Actualités</a>
+                <a href="wall.php?user_id=5">Mur</a>
+                <a href="feed.php?user_id=5">Flux</a>
+                <a href="tags.php?tag_id=1">Mots-clés</a>
+            </nav>
+            <nav id="user">
+                <a href="#">Profil</a>
+                <ul>
+                    <li><a href="settings.php?user_id=5">Paramètres</a></li>
+                    <li><a href="followers.php?user_id=5">Mes suiveurs</a></li>
+                    <li><a href="subscriptions.php?user_id=5">Mes abonnements</a></li>
+                </ul>
+
+            </nav>
+        </header>
+
+        <div id="wrapper" >
+
+            <aside>
+                <h2>Présentation</h2>
+                <p>Bienvenu sur notre réseau social.</p>
+            </aside>
+            <main>
+                <article>
+                    <h2>Inscription</h2>
+                    <?php
+                    /**
+                     * TRAITEMENT DU FORMULAIRE
+                     */
+                    // Etape 1 : vérifier si on est en train d'afficher ou de traiter le formulaire
+                    // si on recoit un champs email rempli il y a une chance que ce soit un traitement
+                    $enCoursDeTraitement = isset($_POST['email']);
+                    if ($enCoursDeTraitement)
+                    {
+                        // on ne fait ce qui suit que si un formulaire a été soumis.
+                        // Etape 2: récupérer ce qu'il y a dans le formulaire @todo: c'est là que votre travaille se situe
+                        // observez le résultat de cette ligne de débug (vous l'effacerez ensuite)
+                        echo "<pre>" . print_r($_POST, 1) . "</pre>";
+                        // et complétez le code ci dessous en remplaçant les ???
+                        $new_email = $_POST['???'];
+                        $new_alias = $_POST['???'];
+                        $new_passwd = $_POST['???'];
+
+
+                        //Etape 3 : Ouvrir une connexion avec la base de donnée.
+                        $mysqli = new mysqli("localhost", "root", "root", "socialnetwork_tests");
+                        //Etape 4 : Petite sécurité
+                        // pour éviter les injection sql : https://www.w3schools.com/sql/sql_injection.asp
+                        $new_email = $mysqli->real_escape_string($new_email);
+                        $new_alias = $mysqli->real_escape_string($new_alias);
+                        $new_passwd = $mysqli->real_escape_string($new_passwd);
+                        // on crypte le mot de passe pour éviter d'exposer notre utilisatrice en cas d'intrusion dans nos systèmes
+                        $new_passwd = md5($new_passwd);
+                        // NB: md5 est pédagogique mais n'est pas recommandée pour une vraies sécurité
+                        //Etape 5 : construction de la requete
+                        $lInstructionSql = "INSERT INTO users (id, email, password, alias) "
+                                . "VALUES (NULL, "
+                                . "'" . $new_email . "', "
+                                . "'" . $new_passwd . "', "
+                                . "'" . $new_alias . "'"
+                                . ");";
+                        // Etape 6: exécution de la requete
+                        $ok = $mysqli->query($lInstructionSql);
+                        if ( ! $ok)
+                        {
+                            echo "L'inscription a échouée : " . $mysqli->error;
+                        } else
+                        {
+                            echo "Votre inscription est un succès : " . $new_alias;
+                            echo " <a href='login.php'>Connectez-vous.</a>";
+                        }
+                    }
+                    ?>                     
+                    <form action="registration.php" method="post">
+                        <input type='hidden'name='???' value='achanger'>
+                        <dl>
+                            <dt><label for='pseudo'>Pseudo</label></dt>
+                            <dd><input type='text'name='pseudo'></dd>
+                            <dt><label for='email'>E-Mail</label></dt>
+                            <dd><input type='email'name='email'></dd>
+                            <dt><label for='motpasse'>Mot de passe</label></dt>
+                            <dd><input type='password'name='motpasse'></dd>
+                        </dl>
+                        <input type='submit'>
+                    </form>
+                </article>
+            </main>
+        </div>
+    </body>
+</html>
---
+++ Niveau2/Niveau2/usurpedpost.php
@@ -0,0 +1,121 @@
+<?php
+session_start();
+?>
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>ReSoC - Post d'usurpateur</title> 
+        <meta name="author" content="Julien Falconnet">
+        <link rel="stylesheet" href="style.css"/>
+    </head>
+    <body>
+        <header>
+            <img src="resoc.jpg" alt="Logo de notre réseau social"/>
+            <nav id="menu">
+                <a href="news.php">Actualités</a>
+                <a href="wall.php?user_id=5">Mur</a>
+                <a href="feed.php?user_id=5">Flux</a>
+                <a href="tags.php?tag_id=1">Mots-clés</a>
+            </nav>
+            <nav id="user">
+                <a href="#">Profil</a>
+                <ul>
+                    <li><a href="settings.php?user_id=5">Paramètres</a></li>
+                    <li><a href="followers.php?user_id=5">Mes suiveurs</a></li>
+                    <li><a href="subscriptions.php?user_id=5">Mes abonnements</a></li>
+                </ul>
+
+            </nav>
+        </header>
+
+        <div id="wrapper" >
+
+            <aside>
+                <h2>Présentation</h2>
+                <p>Sur cette page on peut poster un message en se faisant 
+                    passer pour quelqu'un d'autre</p>
+            </aside>
+            <main>
+                <article>
+                    <h2>Poster un message</h2>
+                    <?php
+                    /**
+                     * BD
+                     */
+                    $mysqli = new mysqli("localhost", "root", "root", "socialnetwork_tests");
+                    /**
+                     * Récupération de la liste des auteurs
+                     */
+                    $listAuteurs = [];
+                    $laQuestionEnSql = "SELECT * FROM users";
+                    $lesInformations = $mysqli->query($laQuestionEnSql);
+                    while ($user = $lesInformations->fetch_assoc())
+                    {
+                        $listAuteurs[$user['id']] = $user['alias'];
+                    }
+
+
+                    /**
+                     * TRAITEMENT DU FORMULAIRE
+                     */
+                    // Etape 1 : vérifier si on est en train d'afficher ou de traiter le formulaire
+                    // si on recoit un champs email rempli il y a une chance que ce soit un traitement
+                    $enCoursDeTraitement = isset($_POST['auteur']);
+                    if ($enCoursDeTraitement)
+                    {
+                        // on ne fait ce qui suit que si un formulaire a été soumis.
+                        // Etape 2: récupérer ce qu'il y a dans le formulaire @todo: c'est là que votre travaille se situe
+                        // observez le résultat de cette ligne de débug (vous l'effacerez ensuite)
+                        echo "<pre>" . print_r($_POST, 1) . "</pre>";
+                        // et complétez le code ci dessous en remplaçant les ???
+                        $authorId = $_POST['???'];
+                        $postContent = $_POST['???'];
+
+
+                        //Etape 3 : Petite sécurité
+                        // pour éviter les injection sql : https://www.w3schools.com/sql/sql_injection.asp
+                        $authorId = intval($mysqli->real_escape_string($authorId));
+                        $postContent = $mysqli->real_escape_string($postContent);
+                        //Etape 4 : construction de la requete
+                        $lInstructionSql = "INSERT INTO posts "
+                                . "(id, user_id, content, created, permalink, post_id) "
+                                . "VALUES (NULL, "
+                                . $authorId . ", "
+                                . "'" . $postContent . "', "
+                                . "NOW(), "
+                                . "'', "
+                                . "NULL);"
+                                ;
+                        echo $lInstructionSql;
+                        // Etape 5 : execution
+                        $ok = $mysqli->query($lInstructionSql);
+                        if ( ! $ok)
+                        {
+                            echo "Impossible d'ajouter le message: " . $mysqli->error;
+                        } else
+                        {
+                            echo "Message posté en tant que :" . $listAuteurs[$authorId];
+                        }
+                    }
+                    ?>                     
+                    <form action="usurpedpost.php" method="post">
+                        <input type='hidden' name='???' value='achanger'>
+                        <dl>
+                            <dt><label for='auteur'>Auteur</label></dt>
+                            <dd><select name='auteur'>
+                                    <?php
+                                    foreach ($listAuteurs as $id => $alias)
+                                        echo "<option value='$id'>$alias</option>";
+                                    ?>
+                                </select></dd>
+                            <dt><label for='message'>Message</label></dt>
+                            <dd><textarea name='message'></textarea></dd>
+                        </dl>
+                        <input type='submit'>
+                    </form>               
+                </article>
+            </main>
+        </div>
+    </body>
+</html>

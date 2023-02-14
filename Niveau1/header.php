2c1e01e06fb740452351a46b4680e576196e6dda
Corentin MONVILLERS <c.monvillers@live.fr>
2023-02-14 10:03:46 +0100

    update

---
+++ Niveau1/header.php
@@ -0,0 +1,18 @@
+<header>
+            <a href='admin.php'><img src="resoc.jpg" alt="Logo de notre réseau social"/></a>
+            <nav id="menu">
+                <a href="news.php">Actualités</a>
+                <a href="wall.php?user_id=5">Mur</a>
+                <a href="feed.php?user_id=5">Flux</a>
+                <a href="tags.php?tag_id=1">Mots-clés</a>
+            </nav>
+            <nav id="user">
+                <a href="#">Profil</a>
+                <ul>
+                    <li><a href="settings.php?user_id=5">Paramètres</a></li>
+                    <li><a href="followers.php?user_id=5">Mes suiveurs</a></li>
+                    <li><a href="subscriptions.php?user_id=5">Mes abonnements</a></li>
+                </ul>
+
+            </nav>
+        </header>
--- Niveau1/subscriptions.php
+++ Niveau1/subscriptions.php
@@ -1,5 +1,6 @@
 <?php
-include 'connect.env' 
+include 'connect.env'; 
+include 'header.php'
 ?>
 <!doctype html>
 <html lang="fr">
@@ -10,30 +11,12 @@ include 'connect.env'
         <link rel="stylesheet" href="style.css"/>
     </head>
     <body>
-        <header>
-            <img src="resoc.jpg" alt="Logo de notre réseau social"/>
-            <nav id="menu">
-                <a href="news.php">Actualités</a>
-                <a href="wall.php?user_id=5">Mur</a>
-                <a href="feed.php?user_id=5">Flux</a>
-                <a href="tags.php?tag_id=1">Mots-clés</a>
-            </nav>
-            <nav id="user">
-                <a href="#">Profil</a>
-                <ul>
-                    <li><a href="settings.php?user_id=5">Paramètres</a></li>
-                    <li><a href="followers.php?user_id=5">Mes suiveurs</a></li>
-                    <li><a href="subscriptions.php?user_id=5">Mes abonnements</a></li>
-                </ul>
-
-            </nav>
-        </header>
         <div id="wrapper">
             <aside>
                 <img src="user.jpg" alt="Portrait de l'utilisatrice"/>
                 <section>
                     <h3>Présentation</h3>
-                    <p>Sur cette page vous trouverez la liste des personnes auxquelles
+                    <p>Sur cette page vous trouverez la liste des abonnements de
                     l'utilisatrice
                     n° <?php echo intval($_GET['user_id']) ?>
                     est abbonée

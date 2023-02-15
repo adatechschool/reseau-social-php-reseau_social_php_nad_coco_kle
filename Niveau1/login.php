c988cbf68c7999263f8e3985d582b6f8321634b8
nadhell <nadhell@live.com>
2023-02-15 15:56:34 +0100

    added page redirection from login to feed when login is a success, but won't return user session id yet

--- Niveau1/login.php
+++ Niveau1/login.php
@@ -23,31 +23,23 @@ session_start();
                     /**
                      * TRAITEMENT DU FORMULAIRE
                      */
-                    // Etape 1 : vérifier si on est en train d'afficher ou de traiter le formulaire
-                    // si on recoit un champs email rempli il y a une chance que ce soit un traitement
+                    // Vérifier si on est en train d'afficher ou de traiter le formulaire
                     $enCoursDeTraitement = isset($_POST['email']);
                     if ($enCoursDeTraitement)
                     {
-                        // on ne fait ce qui suit que si un formulaire a été soumis.
-                        // Etape 2: récupérer ce qu'il y a dans le formulaire @todo: c'est là que votre travail se situe
-                        // observez le résultat de cette ligne de débug (vous l'effacerez ensuite)
-                        echo "<pre>" . print_r($_POST, 1) . "</pre>";
-                        // et complétez le code ci dessous en remplaçant les ???
                         $emailAVerifier = $_POST['email'];
                         $passwdAVerifier = $_POST['motpasse'];
 
-                        //Etape 3 : Ouvrir une connexion avec la base de donnée.
+                        // Ouvrir une connexion avec la base de donnée.
                         $mysqli = new mysqli("localhost", "root", "root", "socialnetwork");
-                        //Etape 4 : Petite sécurité
-                        // pour éviter les injection sql : https://www.w3schools.com/sql/sql_injection.asp
+                        // Petite sécurité pour éviter les injection sql :
                         $emailAVerifier = $mysqli->real_escape_string($emailAVerifier);
                         $passwdAVerifier = $mysqli->real_escape_string($passwdAVerifier);
                         // on crypte le mot de passe pour éviter d'exposer notre utilisatrice en cas d'intrusion dans nos systèmes
                         $passwdAVerifier = md5($passwdAVerifier);
-                        // NB: md5 est pédagogique mais n'est pas recommandée pour une vraies sécurité
-                        //Etape 5 : construction de la requete
+                        // Construction de la requete
                         $lInstructionSql = "SELECT * FROM users WHERE email LIKE '$emailAVerifier'";
-                        // Etape 6: Vérification de l'utilisateur
+                        // Vérification de l'utilisateur
                         $res = $mysqli->query($lInstructionSql);
                         $user = $res->fetch_assoc();
                         if ( ! $user OR $user["password"] != $passwdAVerifier)
@@ -56,10 +48,13 @@ session_start();
                             
                         } else
                         {
-                            echo "Votre connexion est un succès : " . $user['alias'] . ".";
-                            // Etape 7 : Se souvenir que l'utilisateur s'est connecté pour la suite
-                            // documentation: https://www.php.net/manual/fr/session.examples.basic.php
+                            echo "Votre connexion est un succès, " . $user['alias'] . ".";
+                            // Se souvenir que l'utilisateur s'est connecté pour la suite
+                            // pour le moment $_SESSION renvoie un array vide ?
                             $_SESSION['connected_id']=$user['id'];
+                        ?>
+                        <meta http-equiv="refresh" content="1; url=feed.php?= <?php $_SESSION['connected_id'] ?>" />
+                        <?php
                         }
                     }
                     ?>                     

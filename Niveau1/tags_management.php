24c4fe948ae127a90d7270f95d618176442ab64d
nadhell <nadhell@live.com>
2023-02-16 16:15:22 +0100

    cleaned tags management

--- Niveau1/tags_management.php
+++ Niveau1/tags_management.php
@@ -4,9 +4,6 @@
 <?php $explodeTag = explode(",", $post['taglist']) ?>
 <?php $explodeTagId = explode(",", $post['tagidlist']) ?>
 <?php 
-    print_r($post['taglist']) ;
-    print_r($post['tagidlist']) ;
-
 while ($explodeTag) {
 
     $curentTag = array_pop($explodeTag);

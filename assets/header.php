77f2361af6c5ee9f63b767c56e255d007941fe17
Corentin MONVILLERS <c.monvillers@live.fr>
2023-02-21 16:21:46 +0100

    CSS + searchebar

--- assets/header.php
+++ assets/header.php
@@ -1,12 +1,17 @@
 <?php
+include "connect.env"
 ?>
 <header>
-    <a href='../pages/admin.php'><img src="../img/resoc.jpg" alt="Logo de notre réseau social"/></a>
+    <a href='../pages/admin.php'><img src="../img/lineUp.jpg" alt="Logo de notre réseau social"/></a>
         <nav id="menu">
             <a href="../pages/news.php">Actualités</a>
             <a href="../pages/wall.php?user_id=<?php echo $_SESSION['connected_id'] ?>">Mur</a>
             <a href="../pages/feed.php?user_id=<?php echo $_SESSION['connected_id'] ?>">Flux</a>
             <a href="../pages/tags.php?tag_id=1">Mots-clés</a>
+            <a> <form id= "searchbox" action="" method="post"> 
+                <input class="research" type="text" size= "40" name="search" placeholder=" Rechercher un utilisateur">
+                <input class="button-submit" type="submit" value="🔍">
+            </form></a>
         </nav>
         <nav id="user">
             <a href="#">Profil</a>
@@ -18,3 +23,20 @@
             </ul>
         </nav>
 </header>
+        <?php
+        
+// PART SEARCH
+
+    if (isset($_POST['search'])) {
+    $search = $_POST['search'];
+    $laQuestionEnSql = "SELECT * FROM `users` WHERE alias= '$search' ";
+    $lesInformations = $mysqli->query($laQuestionEnSql);
+    $user = $lesInformations->fetch_assoc();
+    if ($user) {
+        header("Location: wall.php?user_id=" . $user['id']);
+    } else {
+        ?> <div class="error_user"> <?php echo "ERROR USER NOT FOUND"; ?></div>
+        <?php
+    }
+}
+?>

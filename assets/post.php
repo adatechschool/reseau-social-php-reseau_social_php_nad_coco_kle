5cd2fdfe41b3aa2b180e3d0c5118f6d9219c3dd5
nadhell <nadhell@live.com>
2023-02-21 17:47:33 +0100

    fixed likes bug

--- assets/post.php
+++ assets/post.php
@@ -22,25 +22,47 @@
         <small>
                 <form method="post" action="">
                     <input type="submit" name="like" value="❤">
+                    <?php
+                    $lInstructionSql = "SELECT * FROM likes
+                    WHERE post_id = " . $post['id'] . " AND user_id =" . $_SESSION['connected_id'];
+                    echo $lInstructionSql;
+
+                    $lesInformations = $mysqli->query($lInstructionSql);
+                    if (!$lesInformations){
+                        echo ("ERROR: " . $mysqli->error);
+                    } else { 
+                        echo ("Brava !");
+                    }
+                    // si le like existe déjà (ligne récupérée dans ma requete précédente) -> no button affiché
+                    // sinon : afficher le bouton <3
+                    ?>
                     <input type="hidden" name ="currentUserId" value="<?php echo $_SESSION['connected_id']?>">
                     <input type="hidden" name="postId" value="<?php echo $post['id'] ?>">
                 </form>
+
                 <?php
 
 $currentUserId = isset ($_POST['currentUserId']) ? $_POST['currentUserId']:0;
 $postId = isset ($_POST['postId']) ? $_POST['postId']:0;
 
-if (isset($_POST['postId']) AND isset($_POST['currentUserId'])) {
-
-    $lInstructionSql = "INSERT INTO likes (post_id, user_id)
-                        SELECT * FROM (SELECT $postId, $currentUserId) AS tmp
-                            WHERE NOT EXISTS (SELECT * FROM likes WHERE postId = $postId AND userId = $currentUserId)
-                                LIMIT 1";
-    $ok = $mysqli->query($lInstructionSql);
-    if (!$ok){
-        echo ("ERROR: " . $mysqli->error);
-    } else { echo ("Merci de répandre l'amour <3")
-        ;}
+if ($postId!=0 AND $currentUserId!=0) {
+    
+    if ( ! $lesInformations)
+    {
+        echo("Échec de la requete : " . $mysqli->error);
+        exit();
+    }
+   
+    while ($like = $lesInformations->fetch_assoc()) {
+        $lInstructionSql = "INSERT INTO `likes` (`id`, `user_id`, `post_id`)
+                            VALUES (NULL, $currentUserId, $postId)";
+        $ok = $mysqli->query($lInstructionSql);
+        if (!$ok){
+            echo ("ERROR: " . $mysqli->error);
+        } else { 
+            echo ("Merci de répandre l'amour <3");
+        }
+    }
 }
 ?>
 
